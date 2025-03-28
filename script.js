4e99fb3c1980f0b758fd0b70b78194f734a7d968
theDAOS <sanjusanthosh46@gmail.com>
2025-03-29 02:16:43 +0530

    darkMode

--- script.js
+++ script.js
@@ -140,4 +140,20 @@ function clock() {
     document.getElementById('footer').innerText = time;
 }
 
+
+function darkMode() {
+    const mode = document.getElementById('mode');
+    const root = document.documentElement;
+
+    if (localStorage.getItem('mode') === "Dark Mode") {
+        mode.innerText = 'Light Mode';
+        localStorage.setItem('mode', 'Light Mode');
+        root.style.setProperty('--color-3', 'ghostwhite');
+    } else {
+        mode.innerText = 'Dark Mode';
+        localStorage.setItem('mode', 'Dark Mode');
+        root.style.setProperty('--color-3', '#000E0F');
+    }
+}
+
 setInterval(clock, 1000);
